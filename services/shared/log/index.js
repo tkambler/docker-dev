77d73686d731841ec831f82294fc2f8829a087c1
ubuntu <tambler@lifeplansinc.com>
2017-11-05 17:06:11 +0000

    Prepand date / time to log entries

--- services/shared/log/index.js
+++ services/shared/log/index.js
@@ -3,12 +3,13 @@
 const _ = require('lodash');
 const fs = require('fs');
 const os = require('os');
+const moment = require('moment');
 
 exports = module.exports = function(logFile) {
 
     return function log(...args) {
 
-        args = args.map((arg) => {
+        args = moment().format('MM-DD-YYYY HH:mm:ss a') + ' :: ' + args.map((arg) => {
             return _.isString(arg) ? arg : JSON.stringify(arg);
         }).join(' ').replace(/^\s+|\s+$/g, '') + os.EOL;
 
